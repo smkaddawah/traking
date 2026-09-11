6b6b04a405a5b5bbeadd1e746644c068b396274a
smkaddawah <operatorsmkaddawah@gmail.com>
2026-09-11 20:46:36 +0700

    gas

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const SUPABASE_KEY = 'sb_publishable_LJn-AYxrSlJ6BzIfhJxh3w_vHHvHupD';
 const db = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // Ganti dengan API Key Google Gemini Anda (Gratis dari Google AI Studio)
-const GEMINI_API_KEY = "AQ.Ab8RN6JYlgjlscGW1UOsWd1CHusRD-N8WTxBw7wdyfB8AtnoYQ";
+const GEMINI_API_KEY = "AQ.Ab8RN6LVpOeineq12dVJH3LWGBIxYLINJoUXI3f-8WWNKNbKJQ";
 
 let currentSaldo = 0;
 
